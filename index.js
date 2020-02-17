28fbec20cc185b37dd0197a5535e36c88bd2e240
verypaleale <55856960+verypaleale@users.noreply.github.com>
2020-02-16 21:59:30 -0600

    added all questions

---
+++ index.js
@@ -0,0 +1,49 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const axios = require("axios");
+
+const writeFileSync = util.promisify(fs.writeFile);
+
+inquirer.prompt([
+    {
+        type: "input",
+        name: "userName",
+        message: "What is your GitHub username"
+    },
+    {
+        type: "input",
+        name: "projectName",
+        message: "What is your project name?",
+    },
+    {
+        type: "input",
+        name: "projDescr",
+        message: "Please write a short description of your project.",
+    },
+    {
+        type: "input", // !CHECK
+        name: "license",
+        message: "What kind of license should your project have?",
+    },
+    {
+        type: "input", // !CHECK FOR DEFAULT OPTIONS (npm i)
+        name: "commDepend",
+        message: "What kind of command should be run to install dependencies?",
+    },
+    {
+        type: "input", // !CHECK TO ADD DEFAULT (npm test)
+        name: "commRun",
+        message: "What type of command should be run to run tests?",
+    },
+    {
+        type: "input",
+        name: "userKnow",
+        message: "What does the user need to know about using the repo?",
+    },
+    {
+        type: "input",
+        name: "userContr",
+        message: "What does the user need to know out contributing to the repo?",
+    }
+]);
